04f62ed43a4a26ec7951358d7b1f7754fdb594b3
Pedro Souza Junior <3471@adm.ucdb.br>
2017-05-09 16:53:31 -0400

    Implementando Conexao com BD utilizando Entity Framework

---
+++ Aplicacao01/Aplicacao01/Contexts/EFContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Models;
+using System.Data.Entity;
+
+namespace Aplicacao01.Contexts
+{
+    public class EFContext: DbContext
+    {
+        public EFContext(): base("Connection_String_Pedro") { }
+
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
